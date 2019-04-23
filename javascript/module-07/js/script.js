b97b186fb9b60c625ff7fdd0572c9f7e9d0df917
Tetiana Melnyk <prostotayna@gmail.com>
2019-04-23 11:52:29 +0300

    module 7 homework

--- javascript/module-07/js/script.js
+++ javascript/module-07/js/script.js
@@ -1,267 +1,221 @@
 'use strict';
 
-// За основу возьми домашнее задание из модуля №5,
-// но теперь необходимо написать ES6 класс.
+// Исходные данные
+// Массив всех пользователей для задания.
 
-class Notepad {
-  /*
-   * Перенеси свойства и методы конструктора в класс
-   *
-   * Замени метод getNotes геттером,
-   * чтобы можно было обратиться как notepad.notes,
-   * для этого создай свойство _notes, в котором храни массив заметок,
-   * а геттер notes возвращает значение этого поля
-   *
-   * Добавь статическое свойство Priority используя ключевое слово static
-   */
-  constructor(notes) {
-    this._notes = notes;
-  }
-
-  get notes() {
-    return this._notes;
-  }
-
-  findNoteById(id) {
-    /*
-     * Ищет заметку в массиве notes
-     *
-     * Принимает: идентификатор заметки
-     * Возвращает: заметку с совпавшим полем id или undefined если ничего не найдено
-     */
-    for (const note of this._notes) {
-      if (note.id === id) {
-        return note;
-      }
-    }
-  }
-
-  saveNote(note) {
-    /*
-     * Сохраняет заметку в массив notes
-     *
-     * Принимает: объект заметки
-     * Возвращает: сохраненную заметку
-     */
-    this._notes.push(note);
-    return note;
-  }
-
-  deleteNote(id) {
-    /*
-     * Удаляет заметку по идентификатору из массива notes
-     *
-     * Принимает: идентификатор заметки
-     * Возвращает: ничего
-     */
-
-    for (let i = 0; i < this._notes.length; i += 1) {
-      const note = this._notes[i];
-
-      if (note.id === id) {
-        this._notes.splice(i, 1);
-      }
-    }
-
-    /*
-     * Альтернативное решение
-     */
-    // const noteToDelete = this.findNoteById(id);
-
-    // if (noteToDelete) {
-    //   const idxNoteToDelete = this.notes.indexOf(noteToDelete);
-    //   this.notes.splice(idxNoteToDelete, 1);
-    // }
-  }
-  updateNoteContent(id, updatedContent) {
-    /*
-     * Обновляет контент заметки
-     * updatedContent - объект с полями вида {имя: значение, имя: значение}
-     * Свойств в объекте updatedContent может быть произвольное количество
-     *
-     * Принимает: идентификатор заметки и объект, полями которого надо обновить заметку
-     * Возвращает: обновленную заметку
-     */
-    const updatedNote = this.findNoteById(id);
-
-    if (!updatedNote) {
-      return;
-    }
-
-    Object.assign(updatedNote, updatedContent);
-
-    return updatedNote;
-
-    /*
-     * Альтернативное решение
-     */
-    // for (const key in updatedContent) {
-    //   updatedNote[key] = updatedContent[key];
-    // }
-
-    /*
-     * Альтернативное решение
-     */
-    // for (let i = 0; i < this.notes.length; i += 1) {
-    //   if (id === this.notes[i].id) {
-    //     this.notes[i] = { ...this.notes[i], ...updatedContent };
-    //     return this.notes[i];
-    //   }
-    // }
-  }
-  updateNotePriority(id, priority) {
-    /*
-     * Обновляет приоритет заметки
-     *
-     * Принимает: идентификатор заметки и ее новый приоритет
-     * Возвращает: обновленную заметку
-     */
-    const updatedNote = this.findNoteById(id);
-
-    if (!updatedNote) {
-      return;
-    }
-
-    updatedNote.priority = priority;
-
-    return updatedNote;
-  }
-  filterNotesByQuery(query) {
-    /*
-     * Фильтрует массив заметок по подстроке query.
-     * Если значение query есть в заголовке или теле заметки - она подходит
-     *
-     * Принимает: подстроку для поиска в title и body заметки
-     * Возвращает: новый массив заметок, контент которых содержит подстроку
-     */
-    query = query.toLowerCase();
-
-    return this._notes.filter(
-      note =>
-        note.title.toLowerCase().includes(query) ||
-        note.body.toLowerCase().includes(query),
-    );
-  }
-  filterNotesByPriority(priority) {
-    /*
-     * Фильтрует массив заметок по значению приоритета
-     * Если значение priority совпадает с приоритетом заметки - она подходит
-     *
-     * Принимает: приоритет для поиска в свойстве priority заметки
-     * Возвращает: новый массив заметок с подходящим приоритетом
-     */
-
-    return this._notes.filter(note => note.priority === priority);
-  }
-
-  static Priority = {
-    LOW: 0,
-    NORMAL: 1,
-    HIGH: 2,
-  };
-}
-
-// Далее идет код для проверки работоспособности класса и созданного экземпляра,
-// вставь его в конец скрипта.
-// Твоя реализация класса Notepad должна проходить этот тест.
-
-const initialNotes = [
+const users = [
   {
-    id: 'id-1',
-    title: 'JavaScript essentials',
-    body:
-      'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
-    priority: Notepad.Priority.HIGH,
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
   },
   {
-    id: 'id-2',
-    title: 'Refresh HTML and CSS',
-    body:
-      'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
-    priority: Notepad.Priority.NORMAL,
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    guid: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
   },
 ];
 
-const notepad = new Notepad(initialNotes);
+// Используя массив объектов пользователей users (дальше в задании),
+// напиши функции которые с помощью перебирающих методов массивов
+// (никаких for, splice и т. д.) выполняют следующие операции.
 
-/*
-  Смотрю что у меня в заметках после инициализации
-*/
-console.log('Все текущие заметки: ', notepad.notes);
+// 1.1. Задание 1
+// Получить массив имен всех пользователей (поле name).
 
-/*
- * Добавляю еще 2 заметки и смотрю что получилось
- */
-notepad.saveNote({
-  id: 'id-3',
-  title: 'Get comfy with Frontend frameworks',
-  body:
-    'First must get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
-  priority: Notepad.Priority.NORMAL,
-});
+const getAllNames = users => users.map(user => user.name);
 
-notepad.saveNote({
-  id: 'id-4',
-  title: 'Winter clothes',
-  body:
-    "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
-  priority: Notepad.Priority.LOW,
-});
+console.log(getAllNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
-console.log('Все текущие заметки: ', notepad.notes);
+// 1.2. Задание 2
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
 
-/*
- * Зима уже близко, пора поднять приоритет на покупку одежды
- */
-notepad.updateNotePriority('id-4', Notepad.Priority.NORMAL);
+const getUsersByEyeColor = (users, color) =>
+  users.filter(user => user.eyeColor === color);
 
-console.log('Заметки после обновления приоритета для id-4: ', notepad.notes);
+console.log(getUsersByEyeColor(users, 'blue'));
+// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-/*
- * Решил что фреймворки отложу немного, понижаю приоритет
- */
-notepad.updateNotePriority('id-3', Notepad.Priority.LOW);
+// 1.3. Задание 3
+// Получить массив имен пользователей по полу (поле gender).
 
-console.log('Заметки после обновления приоритета для id-3: ', notepad.notes);
+const getUsersByGender = (users, gender) =>
+  users.filter(user => user.gender === gender);
 
-/*
- * Решил отфильтровать заметки по слову html
- */
-console.log(
-  'Отфильтровали заметки по ключевому слову "html": ',
-  notepad.filterNotesByQuery('html'),
-);
+console.log(getUsersByGender(users, 'male'));
+// [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-/*
- * Решил отфильтровать заметки по слову javascript
- */
-console.log(
-  'Отфильтровали заметки по ключевому слову "javascript": ',
-  notepad.filterNotesByQuery('javascript'),
-);
+// 1.4. Задание 4
+// Получить массив только неактивных пользователей (поле isActive).
 
-/*
- * Хочу посмотреть только заметки с нормальным приоритетом
- */
-console.log(
-  'Отфильтровали заметки по нормальному приоритету: ',
-  notepad.filterNotesByPriority(Notepad.Priority.NORMAL),
-);
+const getInactiveUsers = users => users.filter(user => !user.isActive);
 
-/*
- * Обновим контент заметки с id-3
- */
-notepad.updateNoteContent('id-3', {
-  title: 'Get comfy with React.js or Vue.js',
-});
+console.log(getInactiveUsers(users));
+// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-console.log(
-  'Заметки после обновления контента заметки с id-3: ',
-  notepad.notes,
-);
+// 1.5. Задание 5
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
 
-/*
- * Повторил HTML и CSS, удаляю запись c id-2
- */
-notepad.deleteNote('id-2');
-console.log('Заметки после удаления с id -2: ', notepad.notes);
+const getUserByEmail = (users, email) =>
+  users.find(user => user.email === email);
+
+console.log(getUserByEmail(users, 'shereeanthony@kog.com'));
+// {объект пользователя Sheree Anthony}
+console.log(getUserByEmail(users, 'elmahead@omatom.com'));
+// {объект пользователя Elma Head}
+
+// 1.6. Задание 6
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет
+// (поле age).
+
+const getUsersWithAge = (users, min, max) =>
+  users.filter(user => user.age >= min && user.age <= max);
+
+console.log(getUsersWithAge(users, 20, 30));
+// [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+// 1.7. Задание 7
+// Получить общую сумму баланса (поле balance) всех пользователей.
+
+const getTotalBalance = users =>
+  users.reduce((sum, user) => sum + user.balance, 0);
+
+console.log(getTotalBalance(users)); // 20916
+
+// 1.8. Задание 8
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersByFriend = (users, name) =>
+  users.filter(user => user.friends.includes(name));
+
+console.log(getUsersByFriend(users, 'Briana Decker'));
+// [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersByFriend(users, 'Goldie Gentry'));
+// [ 'Elma Head', 'Sheree Anthony' ]
+
+// 3. Дополнительное задание
+// ⚠️ ВЫПОЛНЯТЬ ПО ЖЕЛАНИЮ
+
+// Получить массив всех умений всех пользователей (поле skills),
+// при этом не должно быть повторяющихся умений
+// и они должны быть отсортированы в алфавитном порядке
+
+const leaveUniqueSkills = (skills, skill) => {
+  if (!skills.includes(skill)) {
+    skills.push(skill);
+  }
+  return skills;
+};
+
+const getUniqueSkills = users =>
+  users
+    .reduce((skills, user) => {
+      skills.push(...user.skills);
+      return skills;
+    }, [])
+    .reduce(leaveUniqueSkills, [])
+    .sort();
+
+// alternative
+
+// const getUniqueSkills = users =>
+//   users
+//   .map(user => user.skills)
+//   .join()
+//   .split(',')
+//   .reduce(leaveUniqueSkills, [])
+//   .sort();
+
+console.log(getUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit',
+// 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non',
+// 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+// Массив имен (поле name) людей,
+// отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users =>
+  users
+    .sort((a, b) => a.friends.length - b.friends.length)
+    .map(user => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr',
+// 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
