8c476c43368faf9647e10be93c54ad68545fdccc
Tetiana Melnyk <prostotayna@gmail.com>
2019-05-08 13:43:34 +0300

    practise module 8

--- javascript/module-08/js/practise.js
+++ javascript/module-08/js/practise.js
@@ -1,1 +1,205 @@
 'use strict';
+
+/*
+  1
+  Есть список категорий с классом categories (на вкладке HTML).
+  
+  Напишите код, который для каждого элемента li (первая вложенность) 
+  в списке categories выведет в консоль:
+  - Текст непосредственно в нём (название категории)
+  - Количество всех вложенных в него элементов li
+  
+  К примеру:
+    Категория: Животные
+    Количество вложенных li: 4
+*/
+
+const categoriesList = document.querySelector('.categories');
+
+const categories = categoriesList.children;
+
+[...categories].forEach(item => {
+  const category = item.firstChild;
+  const categoryItemsList = category.nextSibling;
+  const categoryItems = categoryItemsList.children;
+  console.log(`Категория: ${category.textContent}
+    Количество вложенных li: ${categoryItems.length}`);
+});
+
+/*
+  2
+  Дан список с классом .skills-list
+	- Найдите первого потомка списка и сделайте его текст красного цвета
+	- Найдите последнего потомка списка и сделайте его текст синего цвета
+*/
+
+const skillsList = document.querySelector('.skills-list');
+
+const firstSkill = skillsList.firstElementChild;
+const lastSkill = skillsList.lastElementChild;
+
+firstSkill.style.color = 'red';
+lastSkill.style.color = 'blue';
+
+/*
+  3
+  Дан ul склассом .list и массив строк. 
+  
+  Вставьте элементы этого массива в ul так, чтобы каждый элемент стоял в своем li.
+*/
+
+const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+const markup = elements.reduce((str, elem) => str + `<li>${elem}</li>`, '');
+
+const list = document.querySelector('.list');
+list.innerHTML = markup;
+
+/*
+  4
+  Напишите скрипт для создания галлереи изображений. 
+  
+  - На вкладке HTML уже есть ul.gallery.
+  - Используйте массив объектов для создания тегов img вложенных в li
+  - Оформление по вкусу, можно и не делать, достаточно чтобы каждое 
+    изображение было 300px по ширине
+  - Добавьте все элементы галлереи в ul.gallery
+*/
+
+const galleryItems = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/1216482/pexels-photo-1216482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Two Brown Hen and One Red Rooster',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/1316294/pexels-photo-1316294.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Macaw Birds',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/41178/africa-animal-big-carnivore-41178.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: '2 Lion on Grass Field during Daytime',
+  },
+];
+
+const galleryMarkup = galleryItems.reduce(
+  (str, elem) =>
+    str + `<li><img src="${elem.url}" alt="${elem.alt}" width="300"></li>`,
+  '',
+);
+
+const gallery = document.querySelector('.gallery');
+
+gallery.innerHTML = galleryMarkup;
+
+gallery.setAttribute(
+  'style',
+  'list-style:none; padding: 20px; background: teal; display: flex; flex-wrap: wrap;',
+);
+
+const galleryItemsList = gallery.children;
+
+[...galleryItemsList].forEach(item =>
+  item.setAttribute('style', 'padding: 20px; border: 1px solid white;'),
+);
+
+/*
+  5
+  Есть список с классом .size-filter из произвольного 
+  количества чекбоксов, каждый из которых содержит 
+  размер одежды в фильтре.
+  
+  Напишите функцию collectInputData(inputs), которая
+  принимает 1 параметр inputs - массив тех инпутов
+  у которых состояние checked.
+  
+  Возвращает массив значений атрибута value.
+*/
+
+const inputs = document.querySelectorAll('input');
+
+const checkedInputs = [...inputs].filter(input =>
+  input.hasAttribute('checked'),
+);
+
+const collectInputData = inputs =>
+  inputs.map(input => input.getAttribute('value'));
+
+console.log(collectInputData(checkedInputs));
+
+/*
+  6
+  Создайте функцию createMovieCard(), которая 
+  создает и возвращает DOM-узел карточки кинофильма.
+  
+  Разметка с классами есть на вкладке HTML.
+  Стили на вкладке CSS.
+  
+  Используйте createElement для создания узлов.
+  Добавьте классы и атрибуты.
+*/
+
+function createMovieCard() {
+  const movieCard = document.createElement('div');
+  movieCard.classList.add('movie');
+
+  const img = document.createElement('img');
+  img.classList.add('movie__image');
+  img.src = 'https://images-na.ssl-images-amazon.com/images/I/41OBAkdAhWL.jpg';
+  img.alt = 'movie image';
+
+  movieCard.appendChild(img);
+
+  const movieInfo = document.createElement('div');
+  movieInfo.classList.add('movie__body');
+
+  movieCard.appendChild(movieInfo);
+
+  const movieTitle = document.createElement('h2');
+  movieTitle.classList.add('movie__title');
+  movieTitle.textContent = 'Betty Blue';
+
+  movieInfo.appendChild(movieTitle);
+
+  const movieDescription = document.createElement('p');
+  movieDescription.classList.add('movie__description');
+  movieDescription.textContent =
+    'A lackadaisical handyman and aspiring novelist tries to support his younger girlfriend as she slowly succumbs to madness.';
+
+  movieInfo.appendChild(movieDescription);
+
+  const movieDate = document.createElement('p');
+  movieDate.classList.add('movie__date');
+  movieDate.textContent = 'Released: 1986-04-09';
+
+  movieInfo.appendChild(movieDate);
+
+  const movieRating = document.createElement('p');
+  movieRating.classList.add('movie__rating');
+  movieRating.textContent = 'Rating: 7.4';
+
+  movieInfo.appendChild(movieRating);
+
+  return movieCard;
+}
+
+const moviesSection = document.querySelector('.movies');
+
+moviesSection.appendChild(createMovieCard());
+moviesSection.appendChild(createMovieCard());
