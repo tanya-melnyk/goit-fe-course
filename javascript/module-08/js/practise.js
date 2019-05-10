bec5bf58a8c6f37fd5484cf5dd7df777db774106
Tetiana Melnyk <prostotayna@gmail.com>
2019-05-10 08:37:13 +0300

    Module 8 practise

--- javascript/module-08/js/practise.js
+++ javascript/module-08/js/practise.js
@@ -202,4 +202,43 @@ function createMovieCard() {
 const moviesSection = document.querySelector('.movies');
 
 moviesSection.appendChild(createMovieCard());
-moviesSection.appendChild(createMovieCard());
+
+/*
+  7
+  В HTML-документе уже есть тег с id="root" (вкладка HTML)
+  
+  Создайте функцию createBoxes(num), которая принимает 1 параметр num - число.
+  
+  Функция создает столько div, сколько указано в num и возвращает их в одном
+  общем контейнере. После чего необходимо повесить результат работы функции
+  в div с id="#root"
+  
+  Каждый div:
+    - Имеет случайный rgb цвет фона
+    - Размеры самого первого div - 30px на 30px.
+    - Каждый следующий div после первого, должен быть шире и выше предыдущего
+      на 10px
+*/
+
+function createBoxes(num) {
+  const container = document.createElement('div');
+  container.classList.add('container');
+  container.setAttribute('style', 'display: flex; flex-wrap: wrap;');
+  let boxSize = 30;
+  const getRandomRgbColor = () => {
+    const getRandomRgbNum = () => Math.floor(Math.random() * 256);
+    return `rgb(${getRandomRgbNum()}, ${getRandomRgbNum()}, ${getRandomRgbNum()})`;
+  };
+  while (num > 0) {
+    num -= 1;
+    const box = document.createElement('div');
+    box.style.backgroundColor = getRandomRgbColor();
+    box.style.width = boxSize + 'px';
+    box.style.height = boxSize + 'px';
+    boxSize += 10;
+    container.appendChild(box);
+  }
+  return container;
+}
+const root = document.querySelector('#root');
+root.appendChild(createBoxes(12));
