ec9f2713bc0e71afdf97676604a1415439564a16
Tetiana Melnyk <prostotayna@gmail.com>
2019-05-24 15:49:47 +0300

    homework 8

--- javascript/module-08/js/script.js
+++ javascript/module-08/js/script.js
@@ -1,211 +1,52 @@
 'use strict';
 
-class Notepad {
-  constructor(notes) {
-    this._notes = notes;
-  }
+import quizData from './quiz-data.js';
 
-  get notes() {
-    return this._notes;
-  }
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('button[type="submit"]');
+const formTitle = document.createElement('h2');
+formTitle.textContent = quizData.title;
+form.insertBefore(formTitle, submitBtn);
 
-  findNoteById(id) {
-    return this._notes.find(note => note.id === id);
-  }
+// Создает одну li с одним ответом
+function createChoiceItem(choice, choiceIdx, questionIdx) {
+  const choiceItem = document.createElement('li');
+  const label = document.createElement('label');
 
-  saveNote(note) {
-    this._notes = [...this._notes, note];
-    return note;
-  }
+  const input = document.createElement('input');
+  input.setAttribute('type', 'radio');
+  input.setAttribute('name', `${questionIdx}`);
+  input.setAttribute('value', `${choiceIdx}`);
 
-  deleteNote(id) {
-    this._notes = this._notes.filter(note => note.id !== id);
-  }
+  label.append(input, choice);
+  choiceItem.appendChild(label);
 
-  updateNoteContent(id, updatedContent) {
-    this._notes = this._notes.map(note =>
-      note.id === id ? { ...note, ...updatedContent } : note,
-    );
-    return this.findNoteById(id);
-  }
-
-  updateNotePriority(id, priority) {
-    this._notes = this._notes.map(note =>
-      note.id === id ? { ...note, priority } : note,
-    );
-    return this.findNoteById(id);
-  }
-
-  filterNotesByQuery(query) {
-    query = query.toLowerCase();
-    return this._notes.filter(
-      note =>
-        note.title.toLowerCase().includes(query) ||
-        note.body.toLowerCase().includes(query),
-    );
-  }
-
-  filterNotesByPriority(priority) {
-    return this._notes.filter(note => note.priority === priority);
-  }
+  return choiceItem;
 }
 
-// const PRIORITY_TYPES = {
-//   LOW: 0,
-//   NORMAL: 1,
-//   HIGH: 2,
-// };
+// Создает секцию с текстом вопроса и списком ol с ответами
+function createQuizItem(questionData, questionIdx) {
+  const quizItem = document.createElement('section');
 
-const PRIORITY_TYPES = {
-  LOW: 'Low',
-  NORMAL: 'Normal',
-  HIGH: 'High',
-};
+  const question = document.createElement('h3');
+  question.textContent = `${questionIdx + 1}. ${questionData.question}`;
 
-const ICON_TYPES = {
-  EDIT: 'edit',
-  DELETE: 'delete',
-  ARROW_DOWN: 'expand_more',
-  ARROW_UP: 'expand_less',
-};
-
-const NOTE_ACTIONS = {
-  DELETE: 'delete-note',
-  EDIT: 'edit-note',
-  INCREASE_PRIORITY: 'increase-priority',
-  DECREASE_PRIORITY: 'decrease-priority',
-};
-
-const initialNotes = [
-  {
-    id: 'id-1',
-    title: 'JavaScript essentials',
-    body:
-      'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
-    priority: PRIORITY_TYPES.HIGH,
-  },
-  {
-    id: 'id-2',
-    title: 'Refresh HTML and CSS',
-    body:
-      'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
-    priority: PRIORITY_TYPES.NORMAL,
-  },
-  {
-    id: 'id-3',
-    title: 'Get comfy with Frontend frameworks',
-    body:
-      'First should get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
-    priority: PRIORITY_TYPES.NORMAL,
-  },
-  {
-    id: 'id-4',
-    title: 'Winter clothes',
-    body:
-      "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
-    priority: PRIORITY_TYPES.LOW,
-  },
-];
-
-const notepad = new Notepad(initialNotes);
-
-const createNoteContent = (title, body) => {
-  const noteContent = document.createElement('div');
-  noteContent.classList.add('note__content');
-
-  const noteTitle = document.createElement('h2');
-  noteTitle.classList.add('note__title');
-  noteTitle.textContent = title;
-
-  const noteBody = document.createElement('p');
-  noteBody.classList.add('note__body');
-  noteBody.textContent = body;
-
-  noteContent.append(noteTitle, noteBody);
-
-  return noteContent;
-};
-
-const createActionButton = (action, iconName) => {
-  const button = document.createElement('button');
-  button.classList.add('action');
-  button.dataset.action = action;
-
-  const icon = document.createElement('i');
-  icon.classList.add('material-icons', 'action__icon');
-  icon.textContent = iconName;
-
-  button.appendChild(icon);
-
-  return button;
-};
-
-const createPrioritySection = priority => {
-  const prioritySection = document.createElement('section');
-  prioritySection.classList.add('note__section');
-
-  const decreaseButton = createActionButton(
-    NOTE_ACTIONS.DECREASE_PRIORITY,
-    ICON_TYPES.ARROW_DOWN,
-  );
-  const increaseButton = createActionButton(
-    NOTE_ACTIONS.INCREASE_PRIORITY,
-    ICON_TYPES.ARROW_UP,
+  const choicesList = document.createElement('ol');
+  const choices = questionData.choices.map((choice, choiceIdx) =>
+    createChoiceItem(choice, choiceIdx, questionIdx),
   );
 
-  const notePriority = document.createElement('span');
-  notePriority.classList.add('note__priority');
-  notePriority.textContent = `Priority: ${priority}`;
+  choicesList.append(...choices);
+  quizItem.append(question, choicesList);
 
-  prioritySection.append(decreaseButton, increaseButton, notePriority);
+  return quizItem;
+}
 
-  return prioritySection;
-};
+// Создает список секций с вопросами
+function createQuizItems(quizData) {
+  return quizData.questions.map((question, i) => createQuizItem(question, i));
+}
 
-const createNoteFooter = priority => {
-  const footer = document.createElement('footer');
-  footer.classList.add('note__footer');
+const quizItems = createQuizItems(quizData);
 
-  const prioritySection = createPrioritySection(priority);
-
-  const editSection = document.createElement('section');
-  editSection.classList.add('note__section');
-
-  const editButton = createActionButton(NOTE_ACTIONS.EDIT, ICON_TYPES.EDIT);
-  const deleteButton = createActionButton(
-    NOTE_ACTIONS.DELETE,
-    ICON_TYPES.DELETE,
-  );
-
-  editSection.append(editButton, deleteButton);
-
-  footer.append(prioritySection, editSection);
-
-  return footer;
-};
-
-const createListItem = note => {
-  const listItem = document.createElement('li');
-  listItem.classList.add('note-list__item');
-  listItem.dataset.id = note.id;
-
-  const noteContainer = document.createElement('div');
-  noteContainer.classList.add('note');
-
-  const noteContent = createNoteContent(note.title, note.body);
-  const noteFooter = createNoteFooter(note.priority);
-
-  noteContainer.append(noteContent, noteFooter);
-  listItem.append(noteContainer);
-
-  return listItem;
-};
-
-const renderNoteList = (listRef, notes) => {
-  const listItems = notes.map(note => createListItem(note));
-  listRef.append(...listItems);
-};
-
-const noteList = document.querySelector('.note-list');
-
-renderNoteList(noteList, notepad.notes);
+submitBtn.before(...quizItems);
