ed4353d560373c9e9af173160a3eb71ae76cf3df
Tetiana Melnyk <prostotayna@gmail.com>
2019-05-30 08:37:53 +0300

    homework 8 class version

--- javascript/module-08/js/script.1.js
+++ javascript/module-08/js/script.1.js
@@ -1,27 +1,5 @@
 'use strict';
 
-// HTML-разметка для вопросов с ответами
-{
-  /* <section>
-  <h3>1. Текст вопроса</h3>
-
-  <ol>
-    <li>
-      <label>
-        <input type="radio" name="question-1" value="0" />
-        Ответ 1
-      </label>
-    </li>
-    <li>
-      <label>
-        <input type="radio" name="question-1" value="1" />
-        Ответ 2
-      </label>
-    </li>
-  </ol>
-</section> */
-}
-
 class Quiz {
   constructor({ title, questions }) {
     this._title = title;
@@ -85,6 +63,112 @@ class Quiz {
   createQuizHTML() {
     return [this.createQuizTitle(), ...this.createQuizItems()];
   }
+
+  // делает кнопку submit активной если все ответы выбраны
+  countAnswers(e) {
+    const form = e.currentTarget;
+
+    // определяет общее количество вопросов
+    const formElements = form.children;
+
+    const questionCount = [...formElements].reduce(
+      (sum, elem) => (elem.className === 'js-question' ? sum + 1 : sum),
+      0,
+    );
+
+    // определяет общее количество ответов, выбранных пользователем
+    const formData = new FormData(e.currentTarget);
+    const userAnswers = {};
+
+    formData.forEach((value, name) => {
+      userAnswers[name] = value;
+    });
+
+    const userAnswersCount = Object.keys(userAnswers).length;
+
+    // делает кнопку submit активной если все ответы выбраны
+    const submitBtn = form.lastElementChild;
+    if (userAnswersCount === questionCount) {
+      submitBtn.removeAttribute('disabled');
+    }
+  }
+
+  // закрашивает ответы в красный или зеленый цвет
+  colorUserAnswers(allInputs) {
+    const rightAnswers = this.questions.map(question => question.answer);
+    let questionNumber = 0;
+
+    allInputs.forEach(input => {
+      const answerText = input.parentNode;
+
+      answerText.classList.remove('bg-green');
+      answerText.classList.remove('bg-red');
+
+      if (input.checked) {
+        if (Number(input.value) === rightAnswers[questionNumber]) {
+          answerText.classList.add('bg-green');
+          questionNumber += 1;
+        } else {
+          answerText.classList.add('bg-red');
+          questionNumber += 1;
+        }
+      }
+    });
+  }
+
+  // считает процент правильных ответов и выводит сообщение с результатом
+  printResultText(userRightAnswersCount, userAnswers, resultText) {
+    const rightAnswersPercentage = Math.round(
+      (userRightAnswersCount / userAnswers.length) * 100,
+    );
+
+    let message = `${rightAnswersPercentage}% правильных ответов.`;
+
+    if (rightAnswersPercentage < 80) {
+      message += ' Тест не пройден. Вам еще нужно подучиться :)';
+      resultText.classList.add('bg-red');
+    } else {
+      message +=
+        ' Поздравляем! Тест пройден. Похоже, вы усердно занимались! :)';
+      resultText.classList.add('bg-green');
+    }
+
+    resultText.textContent = message;
+    resultText.classList.remove('invisible');
+  }
+
+  // считает количество правильных ответов,
+  // закрашивает ответы в красный или зеленый цвет
+  showTestResult(e) {
+    e.preventDefault();
+
+    // считает количество правильных ответов
+    const rightAnswers = this.questions.map(question => question.answer);
+    const formData = new FormData(e.currentTarget);
+    const userAnswers = {};
+
+    formData.forEach((value, name) => {
+      userAnswers[name] = value;
+    });
+
+    const userAnswersArr = Object.values(userAnswers);
+
+    const userRightAnswersCount = userAnswersArr.reduce((sum, answer, i) => {
+      if (Number(answer) === rightAnswers[i]) {
+        sum += 1;
+      }
+      return sum;
+    }, 0);
+
+    // закрашивает ответы в красный или зеленый цвет
+    const form = e.currentTarget;
+    const allInputs = Array.from(form.elements);
+    this.colorUserAnswers(allInputs);
+
+    // выводит результат теста
+    const resultText = form.nextElementSibling;
+    this.printResultText(userRightAnswersCount, userAnswersArr, resultText);
+  }
 }
 
 import quizData from './quiz-data.js';
@@ -95,114 +179,116 @@ const quizHTML = quiz.createQuizHTML();
 const form = document.querySelector('form');
 form.prepend(...quizHTML);
 
+form.addEventListener('change', quiz.countAnswers.bind(quiz));
+form.addEventListener('submit', quiz.showTestResult.bind(quiz));
+
 ////////////////////////////////////////////////
 // СОБЫТИЯ
 ////////////////////////////////////////////////
+// const rightAnswers = quiz.questions.map(question => question.answer);
 
-const rightAnswers = quiz.questions.map(question => question.answer);
+// // делает кнопку submit активной если все ответы выбраны
+// const countAnswers = e => {
+//   const form = e.currentTarget;
 
-// делает кнопку submit активной если все ответы выбраны
-const countAnswers = e => {
-  const form = e.currentTarget;
+//   // определяет общее количество вопросов
+//   const formElements = form.children;
 
-  // определяет общее количество вопросов
-  const formElements = form.children;
+//   const questionCount = [...formElements].reduce(
+//     (sum, elem) => (elem.className === 'js-question' ? sum + 1 : sum),
+//     0,
+//   );
 
-  const questionCount = [...formElements].reduce(
-    (sum, elem) => (elem.className === 'js-question' ? sum + 1 : sum),
-    0,
-  );
+//   // определяет общее количество ответов, выбранных пользователем
+//   const formData = new FormData(e.currentTarget);
+//   const userAnswers = {};
 
-  // определяет общее количество ответов, выбранных пользователем
-  const formData = new FormData(e.currentTarget);
-  const userAnswers = {};
+//   formData.forEach((value, name) => {
+//     userAnswers[name] = value;
+//   });
 
-  formData.forEach((value, name) => {
-    userAnswers[name] = value;
-  });
+//   const userAnswersCount = Object.keys(userAnswers).length;
 
-  const userAnswersCount = Object.keys(userAnswers).length;
+//   // делает кнопку submit активной если все ответы выбраны
+//   const submitBtn = form.lastElementChild;
+//   if (userAnswersCount === questionCount) {
+//     submitBtn.removeAttribute('disabled');
+//   }
+// };
 
-  // делает кнопку submit активной если все ответы выбраны
-  const submitBtn = form.lastElementChild;
-  if (userAnswersCount === questionCount) {
-    submitBtn.removeAttribute('disabled');
-  }
-};
+// // закрашивает ответы в красный или зеленый цвет
+// const colorUserAnswers = allInputs => {
+//   let questionNumber = 0;
 
-// закрашивает ответы в красный или зеленый цвет
-const colorUserAnswers = allInputs => {
-  let questionNumber = 0;
+//   allInputs.forEach(input => {
+//     const answerText = input.parentNode;
 
-  allInputs.forEach(input => {
-    const answerText = input.parentNode;
+//     answerText.classList.remove('bg-green');
+//     answerText.classList.remove('bg-red');
 
-    answerText.classList.remove('bg-green');
-    answerText.classList.remove('bg-red');
+//     if (input.checked) {
+//       if (Number(input.value) === rightAnswers[questionNumber]) {
+//         answerText.classList.add('bg-green');
+//         questionNumber += 1;
+//       } else {
+//         answerText.classList.add('bg-red');
+//         questionNumber += 1;
+//       }
+//     }
+//   });
+// };
 
-    if (input.checked) {
-      if (Number(input.value) === rightAnswers[questionNumber]) {
-        answerText.classList.add('bg-green');
-        questionNumber += 1;
-      } else {
-        answerText.classList.add('bg-red');
-        questionNumber += 1;
-      }
-    }
-  });
-};
+// // считает процент правильных ответов и выводит сообщение с результатом
+// const printResultText = (userRightAnswersCount, userAnswers, resultText) => {
+//   const rightAnswersPercentage = Math.round(
+//     (userRightAnswersCount / userAnswers.length) * 100,
+//   );
 
-// считает процент правильных ответов и выводит сообщение с результатом
-const printResultText = (userRightAnswersCount, userAnswers, resultText) => {
-  const rightAnswersPercentage = Math.round(
-    (userRightAnswersCount / userAnswers.length) * 100,
-  );
+//   let message = `${rightAnswersPercentage}% правильных ответов.`;
 
-  let message = `${rightAnswersPercentage}% правильных ответов.`;
+//   if (rightAnswersPercentage < 80) {
+//     message += ' Тест не пройден. Вам еще нужно подучиться :)';
+//     resultText.classList.add('bg-red');
+//   } else {
+//     message += ' Поздравляем! Тест пройден. Похоже, вы усердно занимались! :)';
+//     resultText.classList.add('bg-green');
+//   }
 
-  if (rightAnswersPercentage < 80) {
-    message += ' Тест не пройден. Вам еще нужно подучиться :)';
-    resultText.classList.add('bg-red');
-  } else {
-    message += ' Поздравляем! Тест пройден. Похоже, вы усердно занимались! :)';
-    resultText.classList.add('bg-green');
-  }
+//   resultText.textContent = message;
+//   resultText.classList.remove('invisible');
+// };
 
-  resultText.textContent = message;
-  resultText.classList.remove('invisible');
-};
+// // считает количество правильных ответов,
+// // закрашивает ответы в красный или зеленый цвет
+// const showTestResult = e => {
+//   e.preventDefault();
 
-// считает количество правильных ответов,
-// закрашивает ответы в красный или зеленый цвет
-const showTestResult = e => {
-  e.preventDefault();
+//   // считает количество правильных ответов
+//   const formData = new FormData(e.currentTarget);
+//   const userAnswers = {};
 
-  // считает количество правильных ответов
-  const formData = new FormData(e.currentTarget);
-  const userAnswers = {};
+//   formData.forEach((value, name) => {
+//     userAnswers[name] = value;
+//   });
 
-  formData.forEach((value, name) => {
-    userAnswers[name] = value;
-  });
+//   const userAnswersArr = Object.values(userAnswers);
 
-  const userAnswersArr = Object.values(userAnswers);
+//   const userRightAnswersCount = userAnswersArr.reduce((sum, answer, i) => {
+//     if (Number(answer) === rightAnswers[i]) {
+//       sum += 1;
+//     }
+//     return sum;
+//   }, 0);
 
-  const userRightAnswersCount = userAnswersArr.reduce((sum, answer, i) => {
-    if (Number(answer) === rightAnswers[i]) {
-      sum += 1;
-    }
-    return sum;
-  }, 0);
+//   // закрашивает ответы в красный или зеленый цвет
+//   const form = e.currentTarget;
+//   const allInputs = Array.from(form.elements);
+//   colorUserAnswers(allInputs);
 
-  // закрашивает ответы в красный или зеленый цвет
-  const form = e.currentTarget;
-  const allInputs = Array.from(form.elements);
-  colorUserAnswers(allInputs);
+//   // выводит результат теста
+//   const resultText = form.nextElementSibling;
+//   printResultText(userRightAnswersCount, userAnswersArr, resultText);
+// };
 
-  // выводит результат теста
-  const resultText = form.nextElementSibling;
-  printResultText(userRightAnswersCount, userAnswersArr, resultText);
-};
-
-form.addEventListener('change', countAnswers);
-form.addEventListener('submit', showTestResult);
+// form.addEventListener('change', countAnswers);
+// form.addEventListener('submit', showTestResult);
