462a4b8531d3574708c8828900c1451c93365e59
Tetiana Melnyk <prostotayna@gmail.com>
2019-06-21 15:27:56 +0300

    ff

--- javascript/module-12/homework/task-2-3.js
+++ javascript/module-12/homework/task-2-3.js
@@ -30,7 +30,8 @@ const makeTransaction = transaction => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve([transaction.id, delay]);
+        const id = transaction.id;
+        resolve({ id, delay });
       } else {
         reject(transaction.id);
       }
@@ -38,8 +39,8 @@ const makeTransaction = transaction => {
   });
 };
 
-const logSuccess = ([id, time]) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, delay }) => {
+  console.log(`Transaction ${id} processed in ${delay}ms`);
 };
 
 const logError = id => {
