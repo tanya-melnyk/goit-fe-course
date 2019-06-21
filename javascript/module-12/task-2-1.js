fbec90454882769f45b438fb4b05261693ca1ceb
Tetiana Melnyk <prostotayna@gmail.com>
2019-06-21 14:23:26 +0300

    homework 12 task 2

--- javascript/module-12/task-2-1.js
+++ javascript/module-12/task-2-1.js
@@ -1,11 +1,16 @@
 'use strict';
 
+// Напиши функцию delay(ms), которая возвращает промис,
+// переходящий в состояние "resolved" через ms миллисекунд.
+// Значением исполнившегося промиса должно быть то кол-во миллисекунд,
+// которое передали во время вызова функции delay.
+
 const delay = ms => {
-  return new Promise(resolve) {
-    setTimeout((ms) => {
-      resollve(ms);
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
     }, ms);
-  }
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
