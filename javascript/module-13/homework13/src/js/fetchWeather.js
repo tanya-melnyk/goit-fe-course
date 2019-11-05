af1e7758f3eb71585a78eaab75c1ac74b88cb61c
Tetiana Melnyk <prostotayna@gmail.com>
2019-11-05 14:14:01 +0200

    Fix cors problem

--- javascript/module-13/homework13/src/js/fetchWeather.js
+++ javascript/module-13/homework13/src/js/fetchWeather.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
 const baseURL = 'http://api.weatherstack.com';
 const APImethod = '/current';
 const APIkey = '?access_key=a2f4a19b05175ece9e4162c1948e6ca4';
@@ -7,7 +8,7 @@ const APIkey = '?access_key=a2f4a19b05175ece9e4162c1948e6ca4';
 export default function fetchWeather(query) {
   const APIquery = `&query=${query}`;
 
-  return fetch(baseURL + APImethod + APIkey + APIquery)
+  return fetch(corsAnywhere + baseURL + APImethod + APIkey + APIquery)
     .then(response => {
       if (response.ok) return response.json();
 
